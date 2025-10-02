34fc5ddae45c415227b9d433574d9fd3779beca1
juliacostaruiz-blip <julia.costa.ruiz@escola.pr.gov.br>
2025-10-02 20:21:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const avanca = document.querySelectorA11('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventListener('click, function'(){
+    button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        const proximoPaso = 'paso-' + this.getAttribute('data-proximo');
-       
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
         atual.classList.remove('ativo');
-        document.getElementById(proximoPaso).classList.add('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
